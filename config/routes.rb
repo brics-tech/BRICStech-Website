c8dac18e4149a3bb426173a62b8d6ea8e9262847
Warda Jawad <wardasobaih@gmail.com>
2025-02-26 13:23:44 +0300

    upload initial

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root "pages#home"
+  get 'login', to: 'sessions#new' # Login page
+  get 'contactt', to: 'contact#contactus' # Contact us page
+  get 'aboutt', to: 'about#aboutus' # About us page
+  get 'stafflogin', to: 'sessions#stafflogin' # About us page
+  get 'partnerlogin', to: 'sessions#partnerlogin' # About us page
+  get 'innotaionlab', to: 'pages#innvotationlab' #  innvotaion lab page
+  get 'solutions', to: 'pages#solutions' # Solutions page
+end
